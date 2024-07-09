ad54fc9be82a6aeb13fd92f004387a14756b3e8e
Pravallika Arigela <71428040+pravallika904@users.noreply.github.com>
2024-07-09 18:23:06 +0530

    Add files via upload

---
+++ Udemy JavaScript/Fundamentals of JavaScript - part 1/Udemy JavaScript/Fundamentals of JavaScript - part 1/6.Taking Decisions of if else statements/script.js
@@ -0,0 +1,27 @@
+/*
+let population = '22';
+let country = 'Portugal';
+
+if (population > 33) 
+    {
+        console.log(`${country}'s population is above average`);
+    } 
+else 
+{
+    console.log(`${country}'s population is ${33 - population} million below average`);
+}
+*/
+
+let population = '130';
+let country = 'Portugal';
+
+if (population > 13) 
+    {
+        console.log(`${country}'s population is above average`);
+    } 
+else 
+{
+    console.log(`${country}'s population is ${13 - population} million below average`);
+}
+
+
