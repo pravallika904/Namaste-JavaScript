4e822f64498c387b75110b6e2971b8e10d5e968e
Pravallika Arigela <71428040+pravallika904@users.noreply.github.com>
2024-07-09 18:27:39 +0530

    Add files via upload

---
+++ Udemy JavaScript/Fundamentals of JavaScript - part 1/5. Strings and Template Literals/script.js
@@ -0,0 +1,8 @@
+let country = 'Portugal';
+let continent = 'Europe';
+let population = '11';
+let language = 'portuguese';
+
+const description = `${ country } is in ${continent}, and its ${population} million people speak ${language}`;
+
+console.log(description);
